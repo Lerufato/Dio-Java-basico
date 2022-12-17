e5d4767ce18d56b5bfed93d374ef0e0382c3d32f
Lerufato <lefurufato@gmail.com>
2022-12-17 20:30:47 -0300

    Inclusão do arquivo Java de aprendizado

---
+++ Aprendendo conceitos/src/edu/Letícia/primeirasemana/ConcatenarNomes.java
@@ -0,0 +1,18 @@
+package edu.Letícia.primeirasemana;
+public class ConcatenarNomes {
+    
+public static void main (String [] args) {
+ 
+    String firstName = "Letícia";
+    String lastName = "Rufato";
+
+    String completedName = completedName(firstName, lastName);
+    System.out.println(completedName);
+
+}
+
+public static String completedName (String firstName, String lastName) {
+    return "Resultado do método: " + firstName.concat(" ").concat(lastName);
+}  
+
+}
