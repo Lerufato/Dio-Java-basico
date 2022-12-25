6284ffbf8b8a745d85625c26dc421ba7404471e1
Lerufato <lefurufato@gmail.com>
2022-12-25 17:23:49 -0300

    Inclusão do arquivo sobre escopo

---
+++ Aprendendo conceitos/src/edu/escopo/Conta.java
@@ -0,0 +1,37 @@
+package edu.escopo;
+
+/*O escopo pode ser entendido como,o ambiente onde uma variável pode ser acessada. 
+Em Java, o escopo de variáveis vai de acordo com o bloco {} onde ela foi declarada. */
+
+public class Conta {
+    //variavel da classe conta
+	double saldo=10.0;
+	
+	public void sacar(Double valor) {
+		//variavel local de método
+		double novoSaldo = saldo - valor; //novoSaldo é uma variável local
+            valor = 2.0;
+
+        System.out.println(novoSaldo);
+	}
+	public void imprimirSaldo(){
+		
+		System.out.println(saldo); //variável saldo estará disponível em toda classe
+		/* 
+		System.out.println(novoSaldo); //somente o método sacar conhece esta variavel */
+	
+	}
+	public double calcularDividaExponencial(){
+		//variável local de método
+		double valorParcela = 50.0;
+		double valorMontante = 0.0; // começando a variável com valor zero
+		for(int x=1; x<=5; x++) {//x variável de escopo de fluxo, esta variável será reiniciada a cada execução for
+			double valorCalculado = valorParcela * x;
+			valorMontante = valorMontante + valorCalculado;
+		}
+		//escopo de fluxo
+		//x e valorCalculado nunca estarão disponíveis fora do for
+		
+		return valorMontante;
+	}
+}
